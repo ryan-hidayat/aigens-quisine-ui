18c22b48e1a3adb728728320e36b5df29bd915ca
Ryan Hidayat <2311rh@proton.me>
2025-12-15 16:25:20 +0700

    Build SCSS foundation

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aq-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+}
